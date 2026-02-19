3658078394d9a45c1ee75ac3700cd0340b0127e0
Derhen David <dedavid@c1r3p11.42lehavre.fr>
2026-02-19 23:02:17 +0100

    Fixed issues with channel join errors

--- srcs/cmds/Join.cpp
+++ srcs/cmds/Join.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Join.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkeromne <student@42lehavre.fr>            +#+  +:+       +#+        */
+/*   By: dedavid <dedavid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 20:43:32 by hkeromne          #+#    #+#             */
-/*   Updated: 2026/02/18 02:07:44 by hkeromne         ###   ########.fr       */
+/*   Updated: 2026/02/19 22:51:33 by dedavid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static bool checkInvite(Channel *channel)
 	if (!channel->getOpInvite())
 		return (true);
 	if (!channel->isInvited(package.client->getNick()))
-		return (package.setError(ERR_NEEDMOREPARAMS), false);
+		return (package.setError(ERR_INVITEONLYCHAN), false);
 	return (true);
 }
 
@@ -27,7 +27,7 @@ static bool	checkKey(Channel *channel)
 	if (!channel->getOpKey())
 		return (true);
 	if (package.cmdData.size() < 2)
-		return (package.setError(ERR_NEEDMOREPARAMS), false);
+		return (package.setError(ERR_BADCHANNELKEY), false);
 	if (!channel->Auth(package.cmdData[JOIN_KEY]))
 		return (package.setError(ERR_BADCHANNELKEY), false);
 	return (true);
@@ -52,15 +52,14 @@ void	Join::Joining(Server &server)
 
 	joinChannel	= package.cmdData[JOIN_CHANNEL];
 	channel		= server.getChannel(joinChannel);
+	package.channel = channel;
 
 	if (channel->isFull())
 		return (package.setError(ERR_CHANNELISFULL));
 	if (!checkInvite(channel) || !checkKey(channel))
 		return ;
-
 	channel->addClient(package.client);
 	package.client->addChannel(channel);
-	package.channel = channel;
 }
 
 void	Join::Create(Server &server)
